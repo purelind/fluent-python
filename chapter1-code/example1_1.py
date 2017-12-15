853e4445d520d07e88bab5cbc852dcd4231ad15d
purelind <kevinsocial@outlook.com>
2017-12-15 19:43:14 +0800

    fix bug: wrong variable

--- chapter1-code/example1_1.py
+++ chapter1-code/example1_1.py
@@ -13,7 +13,7 @@ class FrenchDeck:
                                         for rank in self.ranks]
 
     def __len__(self):
-        return len(self._card)
+        return len(self._cards)
 
     def __getitem__(self, position):
         return self._cards[position]
