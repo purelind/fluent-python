49f77fa0fb86269de5d10e24a4d7cad731c9f9b1
purelind <kevinsocial@outlook.com>
2017-12-17 15:58:05 +0800

    add Example1-2: two-dimensional vector class

---
+++ chapter1/Example1-2.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+    Example1-2.py
+    ~~~~~~~~~~~~~
+    Example 1-2 is a Vector class implementing the operations just described, through the use of the
+    special methods __repr__, __abs__, __add__, and __mul__.
+
+"""
+#: Example 1-2. A simple two-dimensional vector class
+from math import hypot
+
+
+class Vector:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y =y
+
+    def __repr__(self):
+        return 'Vector(%r, %r)' % (self.x, self.y)
+
+    #: hypot: 平方和的平方根
+    def __abs__(self):
+        return hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    #: scalar: 标量
+    def __mul__(self, scalar):
+        return Vector(self.x*scalar, self.y*scalar)
+
+
+
+
+
