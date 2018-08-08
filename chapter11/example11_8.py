ccc41f84a1b2076adfaa74fd7e9b85501b4e0e13
purelind <kevinsocial@outlook.com>
2018-08-08 23:31:16 +0800

    update

---
+++ chapter11/example11_8.py
@@ -0,0 +1,27 @@
+import collections
+
+Card = collections.namedtuple("Card", ["rank", "suit"])
+
+
+class FrenchDeck2(collections.MutableSequence):
+    ranks = [str(n) for n in range(2, 11)] + list("JQKA")
+    suits = "spades diamonds clubs hearts".split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                       for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+    def __setitem__(self, key, value):
+        self._cards[key] = value
+
+    def __delitem__(self, key):
+        del self._cards[key]
+
+    def insert(self, position, value):
+        self._cards.insert(position, value)
